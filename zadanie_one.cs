88420922d39e110180bd25512a473ce1989a6cf7
mimmarry <105546871+mimmarry@users.noreply.github.com>
2022-07-13 02:43:17 +0300

    Add files via upload

---
+++ zadanie_one.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        double firstValue, secondValue;
+        Console.WriteLine("Ввдите первое число");
+        firstValue = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ввдите 2 число");
+        secondValue = double.Parse(Console.ReadLine());
+        double result = (firstValue + secondValue) / 2;
+        Console.WriteLine(result);
+    }
+
+}
